34e9c00031700c4ea3edd88f7c4f2ceb63d3d350
Mahmoud Adam <mahmoud.khalifa@lognlabs.com>
2016-10-22 16:23:48 +0200

    Added base projet

---
+++ Iqama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Iqama
+//
+//  Created by Mahmoud Adam on 10/22/16.
+//  Copyright © 2016 lognlabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
