9c189281199d9839d8de80333fc274068a656cf2
Mahmoud Adam <mahmoud.khalifa@lognlabs.com>
2016-11-20 15:39:06 +0100

    [Refactor] used formatted time instead of time since midnight in Pray constructor

--- Iqama/Pray.swift
+++ Iqama/Pray.swift
@@ -19,30 +19,20 @@ public enum PrayType {
 class Pray: NSObject {
     // Constants
     static let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-    static let dateFormatter = NSDateFormatter()
     
     // instance variables
     private var prayType: PrayType
-    private var prayDateTime: NSDate
+    private var formattedTime: String
     
     
-    override class func initialize() {
-        cal.timeZone = Constants.timeZone
-        dateFormatter.dateFormat = "HH:mm"
-        dateFormatter.timeZone = Constants.timeZone
-    }
-    
-    init(prayType: PrayType, date: NSDate, timeSinceMidnight: Double) {
+    init(prayType: PrayType, formattedTime: String) {
         self.prayType = prayType
-        let startOfDay = Pray.cal.startOfDayForDate(date)
-        self.prayDateTime = startOfDay.dateByAddingTimeInterval(timeSinceMidnight)
+        self.formattedTime = formattedTime
         super.init()
     }
 
     
     func getFormatedPrayTime() -> String {
-        
-        let formattedTime = Pray.dateFormatter.stringFromDate(prayDateTime)
         return formattedTime
     }
     
@@ -74,5 +64,27 @@ class Pray: NSObject {
         case .Isha:
             return "العشاء"
         }
+    }
+    
+    func getPrayDateTime(date: NSDate) -> NSDate {
+        let startOfDay = Pray.cal.startOfDayForDate(date)
+        let timeSinceMidnight = getTimeSinceMidnight(prayType, formatedTime: formattedTime)
+        let prayDateTime = startOfDay.dateByAddingTimeInterval(timeSinceMidnight)
+        return prayDateTime
+    }
+    
+    
+    private func getTimeSinceMidnight(prayType: PrayType, formatedTime: String) -> NSTimeInterval {
+        let timeComponents = formatedTime.componentsSeparatedByString(":")
+        guard timeComponents.count == 2 else {
+            return 0
+        }
+        
+        let hours = Double(timeComponents[0])! + (prayType == .Fajir ? 0 : 12)
+        let minutes = Double(timeComponents[1])!
+        
+        let timeSinceMidnight = hours * 3600.0 + minutes * 60.0
+        return timeSinceMidnight
+        
     }
 }
